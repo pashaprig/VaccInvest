3e7db9952a242fe28b26b6fe35d9c97babf22a8e
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2021-10-13 13:09:55 +0300

    Form top mibile styling

--- source/js/script.js
+++ source/js/script.js
@@ -1,45 +1,45 @@
-class Tabs{
-  constructor(){
-    this.tabList = document.querySelectorAll('.places-tab__tabItem');
-    this.contentList = document.querySelectorAll('.places-tab__contentItem');
-    let nav = document.querySelector('.places-tab');
+// class Tabs{
+//   constructor(){
+//     this.tabList = document.querySelectorAll('.places-tab__tabItem');
+//     this.contentList = document.querySelectorAll('.places-tab__contentItem');
+//     let nav = document.querySelector('.places-tab');
 
-    nav.addEventListener('click', e => this.show(e));
+//     nav.addEventListener('click', e => this.show(e));
 
-    this.setIndex();
-  }
+//     this.setIndex();
+//   }
 
-  show(e){
-    let t = e.target;
-    if (!t.classList.contains('places-tab__tabItem')) return;
-    this.removePrev();
+//   show(e){
+//     let t = e.target;
+//     if (!t.classList.contains('places-tab__tabItem')) return;
+//     this.removePrev();
 
-    let index = t.getAttribute('data-index');
-    let content = document.querySelector('.places-tab__contentItem[data-index="'+index+'"]');
+//     let index = t.getAttribute('data-index');
+//     let content = document.querySelector('.places-tab__contentItem[data-index="'+index+'"]');
 
-    t.classList.add('places-tab__tabItem--active');
-    content.classList.add('places-tab__contentItem--active');
-  }
+//     t.classList.add('places-tab__tabItem--active');
+//     content.classList.add('places-tab__contentItem--active');
+//   }
 
-  setIndex(){
-    for (let i = 0; i < this.tabList.length; i++){
-      this.tabList[i].setAttribute('data-index', i);
-      this.contentList[i].setAttribute('data-index', i);
-    }
-  }
+//   setIndex(){
+//     for (let i = 0; i < this.tabList.length; i++){
+//       this.tabList[i].setAttribute('data-index', i);
+//       this.contentList[i].setAttribute('data-index', i);
+//     }
+//   }
 
-  removePrev(){
-    for (let i = 0; i < this.tabList.length; i++){
-      this.tabList[i].classList.remove('places-tab__tabItem--active');
-      this.contentList[i].classList.remove('places-tab__contentItem--active');
-    }
-  }
+//   removePrev(){
+//     for (let i = 0; i < this.tabList.length; i++){
+//       this.tabList[i].classList.remove('places-tab__tabItem--active');
+//       this.contentList[i].classList.remove('places-tab__contentItem--active');
+//     }
+//   }
 
-}
+// }
 
-document.addEventListener('DOMContentLoaded', ()=>{
-  let tabs = new Tabs();
-})
+// document.addEventListener('DOMContentLoaded', ()=>{
+//   let tabs = new Tabs();
+// })
 
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
