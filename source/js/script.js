b6a6cfc87f0f486b306efc4ec773c5a6086744fb
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2021-10-25 14:02:56 +0300

    Animation for mobile menu

--- source/js/script.js
+++ source/js/script.js
@@ -47,3 +47,21 @@ calculateValue.addEventListener('input', function () {
   }
 })
 
+
+//localStorage
+
+if (window.localStorage) {
+  var elements = document.querySelectorAll('[name]');
+
+  for (var i = 0, length = elements.length; i < length; i++) {
+    (function(element) {
+      var name = element.getAttribute('name');
+
+      element.value = localStorage.getItem(name) || '';
+
+      element.onkeyup = function() {
+        localStorage.setItem(name, element.value);
+      };
+    })(elements[i]);
+  }
+}
